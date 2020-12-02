1baecb4171480ad7e76c4575baa31ee79144b7ee
Ani Jekova <angelina.jekova@rubberduck.xyz>
2020-12-02 16:30:16 +0200

    update

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -3,15 +3,10 @@ import { StyleSheet, View, Text } from 'react-native';
 import TestPackage from 'test-package';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    TestPackage.multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <Text>Result: </Text>
+      <TestPackage />
     </View>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
-// export default {
-//   multiply(a: number, b: number) {
-//     return Promise.resolve(a * b);
-//   },
-// };
-
 export default class CustomComponent extends Component {
   constructor(props: any) {
     super(props);
