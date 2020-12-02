7af1e176ca9c11c5835213c5cafb4fc718648a31
Ani Jekova <angelina.jekova@rubberduck.xyz>
2020-12-02 16:05:36 +0200

    index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,22 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+// export default {
+//   multiply(a: number, b: number) {
+//     return Promise.resolve(a * b);
+//   },
+// };
+
+export default class CustomComponent extends Component {
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>meow</Text>
+      </View>
+    );
+  }
+}
